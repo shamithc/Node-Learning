0bd8a1267221158ed718e1df99f090443b79ac9f
Shamith <shamith.c@bridge-india.in>
2014-12-04 17:06:49 +0530

    Post data to /posts url.

--- Blog/app.js
+++ Blog/app.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
+var qs = require('querystring')
 
 var newPostFormHtml = fs.readFileSync('views/post/new.html');
 
@@ -16,11 +17,40 @@ function render404(request, response){
 	response.end('File not found!')
 }
 
+function postPost(request, response){
+	parseBody(request, function(body){
+		var post = {
+			title: body.title,
+			content: body.content
+		}
+		console.log("Title:" + post.title + "Content:"+ post.content);
+	})
+	response.end();
+}
+
+function parseBody(request, callback){
+	var body = '';
+	request.on('data', function(chunk){
+		body += chunk;
+	});
+	request.on('end', function(){
+		callback(qs.parse(body))
+	})
+}
+
+// Router functions
+var newPostFormRegex = new RegExp('^/posts/new/?$');
+var newPostRegex = new RegExp('^/posts');
+
+
+// Server functions
 var httpServer = http.createServer(function(request, response){
-	var newPostFormRegex = new RegExp('^/posts/new/?$');
+
 	var pathName = url.parse(request.url).pathname;
 	if(newPostFormRegex.test(pathName)){
 		renderNewPost(request, response)
+	}else if(newPostRegex.test(pathName)){
+		postPost(request, response)
 	}else{
 		render404(request, response)
 	}
